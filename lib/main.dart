118e0bf0c0b5b2672f13a0a42d151e936a6cee13
mahmoudradwan11 <102020555+mahmoudradwan11@users.noreply.github.com>
2022-12-04 18:55:23 +0200

    copy rights v1

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class MyApp extends StatelessWidget {
               darkTheme: darkTheme,
               home: AnimatedSplashScreen(
                 splash: logo(),
-                duration: 2000,
+                duration: 3000,
                 nextScreen: startWidget!,
                 splashTransition: SplashTransition.scaleTransition,
                 backgroundColor: Colors.white,
